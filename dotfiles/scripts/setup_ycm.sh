2dc872d50762f639df577328a251fed999d0333b
Kabir Sohel <kabir.sohel@booking.com>
2016-08-24 16:04:24 +0200

    Ycm did can exist

--- dotfiles/scripts/setup_ycm.sh
+++ dotfiles/scripts/setup_ycm.sh
@@ -2,7 +2,7 @@
 sudo yum install cmake
 vim +PluginInstall +qall
 cd ~/
-mkdir ycm_build
+mkdir -p ycm_build
 cd ycm_build
 cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 make ycm_support_libs
