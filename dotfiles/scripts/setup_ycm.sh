96521aa22f59b488bed646ba228443fdf090ef06
Kabir Sohel <kabir.sohel@booking.com>
2016-08-26 22:49:58 +0200

    Ycm needs to built

--- dotfiles/scripts/setup_ycm.sh
+++ dotfiles/scripts/setup_ycm.sh
@@ -5,5 +5,6 @@ cd ~/
 mkdir -p ycm_build
 cd ycm_build
 cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+cmake --build . --target ycm_core --config Release
 make ycm_support_libs
 
